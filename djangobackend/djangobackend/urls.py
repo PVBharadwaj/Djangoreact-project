0fe287c4d6bf9da2ff2c6eb3bc8d214e84d5852f
PVBharadwaj <vinathibharadwaj@gmail.com>
2025-01-25 01:00:27 +0530

    changes

--- djangobackend/djangobackend/urls.py
+++ djangobackend/djangobackend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home  # Import your view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'), 
     path('api/', include('api.urls'))
 ]
