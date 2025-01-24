7a600eb88f996bc5fc10c3fdfd6277afc588b09d
PVBharadwaj <vinathibharadwaj@gmail.com>
2025-01-25 01:56:16 +0530

    changes

--- djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/settings.py
@@ -142,6 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
    'http://localhost:3000',
+   'https://djangostudentrecord.netlify.app/',
 ]
 
 REST_FRAMEWORK = {
